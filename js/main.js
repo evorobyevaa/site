12a954ac2db6f30ad5211b2875bcf998ee9b8c43
evorobyevaa <82096483+evorobyevaa@users.noreply.github.com>
2021-12-07 11:00:16 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function(){
   $('.slider__box').slick({
-    prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/arrow-l.svg" alt="arrow">',
-    nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow-r.svg" alt="arrow">'
+    prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/Arrow-l.svg" alt="arrow">',
+    nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/Arrow-r.svg" alt="arrow">'
   });
 
   $('.menu-btn').on('click', function(){
